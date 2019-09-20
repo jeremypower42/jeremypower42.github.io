00f886d25c8d4bf5b67f5c0ffb4d8bc82666575c
Jeremy Power <jeremy.power42@gmail.com>
2019-09-19 21:53:49 -0600

    Added last Update date and time

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+/* code to get current date and post to html file. */
+let dayNames = [
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+
+];
+
+let months = [
+
+    "Janurary",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+
+];
+
+let d = new Date();
+let dayName = dayNames[d.getDay()];
+let monthName  = months[d.months()];
+let fullDate = dayName + ", " + monthName + " " + d.getDate()
++ ", " + d.getFullYear();
+
+document.getElementById("currentdate").innerHTML = fullDate;
+
+try {
+    let options = {
+        weekday: "long";
+        day = "nemeric";
+        month = "long";
+        year = "numeric";
+    };
+    document.getElementById(
+        "currentdate2").innerHTML = new Date().toLocaleDateString("en-US", options);}
+        catch (e){
+            alert("Error");
+        }
+
+    )
+}
+
+
+
