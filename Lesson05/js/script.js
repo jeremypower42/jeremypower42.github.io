26bd712db185ef724a44e755de880ea963245de7
Jeremy Power <jeremy.power42@gmail.com>
2019-10-21 22:07:28 -0600

    updated all lesson, started work on lesson05 added favicon

--- Lesson05/js/script.js
+++ Lesson05/js/script.js
@@ -1,3 +1,8 @@
 function toggleMenu() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+
+document.write ('<p>Current time is: <span id="date-time">', new Date().toLocaleString(), '<\/span>.<\/p>')
+            if (document.getElementById) onload = function () {
+                setInterval ("document.getElementById ('date-time').firstChild.data = new Date().toLocaleString()", 50)
+            }
