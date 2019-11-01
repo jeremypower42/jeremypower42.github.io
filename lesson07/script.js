c7082df5adf6789b9bcd73851d391062b8272dde
Jeremy Power <jeremy.power42@gmail.com>
2019-11-01 14:34:40 -0600

    added js for Lazy Loading

---
+++ lesson07/script.js
@@ -0,0 +1,29 @@
+const imagestoLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = { 
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');};
+
+};
+
+if('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items,observer) => {
+        items.forEach((item) => {   
+        });
+    }, imgOptions);
+
+    imagestoLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+}
+
+else {
+    imagestoLoad.forEach((img) => {
+        loadImages(img);
+    });
+
+}
